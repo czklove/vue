87735c5692096ea132f85363077dc30601e5e989
czklove <czklove@126.com>
2019-01-25 20:49:08 +0800

    vuecli

---
+++ work/dome/my-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <hello></hello>
+    <img src="./assets/logo.png" alt="logo"/>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+import hello from './components/HelloWorld.vue'
+export default {
+  data: function () {
+    return {
+      msg: 'app.vue'
+    }
+  },
+  components: {
+    hello
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ work/dome/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default{
+  data: function () {
+    return {
+      msg: '来自鬼顾的ni'
+    }
+  }
+}
+</script>
+
+<style>
+  h1{color: red;}
+</style>
